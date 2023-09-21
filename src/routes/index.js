b3839896f49004e13f0b7e196f0beba639c73af4
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-21 13:00:02 -0300

    user model rectified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const auth = require('../controllers/auth');
-const admin = require('../controllers/admin');
 const user = require('../controllers/get-user');
 const account = require('../controllers/account');
 const innerLogin = require('../controllers/login-inner');
@@ -10,7 +9,6 @@ const innerSignup = require('../controllers/signup-inner');
 const googleSignup = require('../controllers/signup-google');
 
 const mercadopago = require("../integrations/mercadopago.js");
-const yt = require('../controllers/yt');
 
 router.use("/auth", auth);
 router.use("/user", user);
